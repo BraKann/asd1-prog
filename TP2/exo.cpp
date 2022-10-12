a802f26fe7c04527331455182dd18fbafedab02a
Christopher CHIABERGE <E195122P@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2022-10-12 18:27:51 +0200

    add exo1tp2

---
+++ TP2/exo.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<math.h>
+#include<cstring>
+using namespace std;
+
+struct Note {
+    int nbQuestions;
+    char* reponses;
+    char note; 
+};
+
+struct Classe {
+    int nbMax;
+    int nbEleve;
+    string* etu;
+    Note* noteEtu;
+};
+
+void initNote(Note* n){
+    cin >> n->nbQuestions;
+    n->reponses = new char[n->nbQuestions];
+
+    for(int i = 0; i < n->nbQuestions; i++){
+        int k = random() % 2;
+        if(k == 0){
+            n->reponses[i] = ' ';
+        } else if(k == 1){
+            n->reponses[i] = 'T';
+        } else {
+            n->reponses[i] = 'F';
+        }
+    }
+
+    n->note = 'A';
+
+}
+
+void initClasse(Classe* c){
+    cin >> c->nbMax;
+    c->nbEleve = 0;
+    
+    c->etu = new string[c->nbMax];
+}
+
+int main() {
+    Note F1;
+    Classe C1;
+
+
+
+}
